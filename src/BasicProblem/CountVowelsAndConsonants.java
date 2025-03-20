b637bda4636effbdfd71931c4a71e34be3ae3cdc
praveengak <arugilpraveen@gmail.com>
2025-03-20 08:29:33 +0530

    Improving code for the CountVowelsAndConsonants

--- src/BasicProblem/CountVowelsAndConsonants.java
+++ src/BasicProblem/CountVowelsAndConsonants.java
@@ -1,25 +1,27 @@
 package BasicProblem;
 
 import java.util.Scanner;
+import java.util.Set;
 
 public class CountVowelsAndConsonants {
 
     public void toCountVowelsAndConsonants(String words){
-        int count = words.length();
-        System.out.println("Lenght of words: "+count);
-        char[] vowels = {'a','e','i','o','u'};
+
+        Set<Character> vowels = Set.of('a','e','i','o','u');
         int vowelWords=0;
         int consonants=0;
 
-        for(int i=0;i<count;i++){
-            for(int j=0;j<5;j++){
-                if(words.charAt(i) == vowels[j]){
+        for(char ch : words.toLowerCase().toCharArray()){
+            if(Character.isLetter(ch)){
+                if(vowels.contains(ch)){
                     vowelWords+=1;
+                }
+                else{
+                    consonants+=1;
                 }
             }
         }
 
-        consonants = count - vowelWords;
         System.out.println("Number of vowels: "+vowelWords);
         System.out.println("Number of consonants: "+consonants);
 
